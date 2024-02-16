46e28bc136e5ec9a6b24670bbce0602c7c54f442
sumit kumar <sumokhan76342@gmail.com>
2024-02-16 23:56:09 +0530

    not complete

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import HeroSection from './components/HeroSection';
+import Services from './components/Services';
+import Trusted from './components/Trusted';
 const Home = () => {
 
   const data = {
     name: "Sumit Store"
   }
-  return <HeroSection myData={data} />
+  return <>
+    <HeroSection myData={data} />
+    <Services />
+    <Trusted />
+  </>
+
 };
 
 
---
+++ src/components/Trusted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Trusted() {
+  return (
+    <div>
+      This is trusted website
+    </div>
+  )
+}
+
+export default Trusted
