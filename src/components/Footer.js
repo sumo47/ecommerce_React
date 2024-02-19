193699d4fdbbca51cb02d6d8b8815ca5310cd469
sumit kumar <sumokhan76342@gmail.com>
2024-02-20 00:09:38 +0530

    mobile number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -62,7 +62,7 @@ const Footer = () => {
                         </div>
                         <div className="footer-contact">
                             <h3>Call Us</h3>
-                            <h3>+91 7481917452</h3>
+                            <a href="tel:7481917452"><h3>+91 7481917452</h3></a>
                         </div>
                     </div>
                     <div className="footer-bottom--section">
