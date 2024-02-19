bf5ad8b4efac5057affdad9a97568e650bc22ee5
sumit kumar <sumokhan76342@gmail.com>
2024-02-19 11:22:05 +0530

    Footer.js completed

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {ErrorPage} from "./ErrorPage";
 import {GlobalStyle} from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -43,8 +44,8 @@ const App = () => {
       <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
       <Route path='/cart' element={<Cart/>}/>
       <Route path='*' element={<ErrorPage/>}/>
-
     </Routes>
+    <Footer/>
     </BrowserRouter>
     </ThemeProvider>
   )
