8e819018e5903e300da293666de0543e28896586
sumit kumar <sumokhan76342@gmail.com>
2024-02-25 00:05:46 +0530

    used useReducer hook and context api in about page

--- src/About.js
+++ src/About.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
 import { useGlobalHookAppProvider } from './components/context/AppContext'
+import { Button } from './styles/Button'
 
 export const About = () => {
 
 
-  const {about} = useGlobalHookAppProvider()
+  const { about, state, dispatch } = useGlobalHookAppProvider()
   return (
     <>
-    <HeroSection myData={about}/>
+      <h1>{state}</h1>
+      <Button onClick={() => dispatch({ type: "INCREMENT" })}>Increment</Button>
+      <Button onClick={() => dispatch({ type: "DECREMENT" })}>Decrement</Button>
+      <HeroSection myData={about} />
     </>
   )
 }
--- src/components/context/AppContext.js
+++ src/components/context/AppContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from 'react'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 import axios from 'axios'
 
 const AppContext = createContext()
@@ -8,18 +8,34 @@ const AppProvider = ({ children }) => {
     const about = { name: "Sumit Sah" }
     const API = "https://api.pujakaitem.com/api/products"
 
-
     const getProduct = async (url) => {
-        const res =  await axios.get(url);
-        console.log(res.data)
+        const res = await axios.get(url)
+        const products = res.data
+        console.log(products)
     }
-
-
     useEffect(() => {
         getProduct(API)
     }, [])
 
-    return <AppContext.Provider value={{ about }}>
+    let initialState = 0;
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "INCREMENT":
+                state++
+                break;
+            case "DECREMENT":
+                state--
+                break;
+            default:
+                break;
+        }
+        console.log(state, action)
+        return state;
+    };
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return <AppContext.Provider value={{ about, state, dispatch }}>
         {children}
     </AppContext.Provider>
 }
