c87c94cd5c367e19448e147793dab7594073c409
sumit kumar <sumokhan76342@gmail.com>
2024-02-24 16:25:52 +0530

    created first api getproduct

--- src/About.js
+++ src/About.js
@@ -3,15 +3,12 @@ import HeroSection from './components/HeroSection'
 import { useGlobalHookAppProvider } from './components/context/AppContext'
 
 export const About = () => {
-  const data = {
-    name:"Sumit Ecommerce"
-  }
+
 
   const {about} = useGlobalHookAppProvider()
   return (
     <>
-    <h1>{about}</h1>
-    <HeroSection myData={data}/>
+    <HeroSection myData={about}/>
     </>
   )
 }
--- src/components/context/AppContext.js
+++ src/components/context/AppContext.js
@@ -1,13 +1,25 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect } from 'react'
+import axios from 'axios'
 
 const AppContext = createContext()
 
 const AppProvider = ({ children }) => {
 
+    const about = { name: "Sumit Sah" }
+    const API = "https://api.pujakaitem.com/api/products"
 
-    const about = {name:"sumit sah"}
 
-    return <AppContext.Provider value={{about}}> 
+    const getProduct = async (url) => {
+        const res =  await axios.get(url);
+        console.log(res.data)
+    }
+
+
+    useEffect(() => {
+        getProduct(API)
+    }, [])
+
+    return <AppContext.Provider value={{ about }}>
         {children}
     </AppContext.Provider>
 }
