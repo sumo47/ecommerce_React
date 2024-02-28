fd28912b4edd690d316af1cdebaa0eb01a80e727
sumit kumar <sumokhan76342@gmail.com>
2024-02-28 23:26:00 +0530

    FormatPrice function added

--- src/About.js
+++ src/About.js
@@ -3,11 +3,6 @@ import HeroSection from './components/HeroSection'
 import { useGlobalHookAppProvider } from './components/context/AppContext'
 
 export const About = () => {
-
-  const data = {
-    name:"Sumit sah"
-  }
-
   const { about } = useGlobalHookAppProvider()
   return (
     <>
