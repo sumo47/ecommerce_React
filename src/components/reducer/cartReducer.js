e030966f73f6cf309ce2e1fc1679347900f6ee6e
sumit kumar <sumokhan76342@gmail.com>
2024-04-06 01:26:35 +0530

    handle exist product in cart

--- src/components/reducer/cartReducer.js
+++ src/components/reducer/cartReducer.js
@@ -4,22 +4,52 @@ function cartReducer(state, action) {
     case 'ADD_TO_CART':
       let { id, color, amount, product } = action.payload
 
-      let Add_Items = {
-        id: id + color,
-        name: product.name,
-        color,
-        amount,
-        image: product.image[0].url,
-        price: product.price,
-        max: product.stock
+      // tackle exist item in cart 
+      //!have to read again
+      let existItem = state.cart_Items.find((item) => {
+        return item.id === id + color
+      })
+      // console.log(state, "exist item in cart")
 
+      if (existItem) {
+        let updatedProduct = state.cart_Items.map((item) => {
+          if (item.id === id + color) {
+            let netAmount = item.amount + amount;
+            if (netAmount >= item.max) {
+              netAmount = item.max
+            }
+            return {
+              ...item,
+              amount: netAmount
+            }
+          } else {
+            return item
+          }
+        })
+        return {
+          ...state
+          , cart_Items: updatedProduct
+        }
       }
-      // state.cart_Items.push(Add_Items)
-      return {
-        ...state
-        , cart_Items: [...state.cart_Items, Add_Items]
-      };
+      else {
 
+        let Add_Items = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.image[0].url,
+          price: product.price,
+          max: product.stock
+
+        }
+        // state.cart_Items.push(Add_Items)
+        return {
+          ...state
+          , cart_Items: [...state.cart_Items, Add_Items]
+
+        }
+      }
     case 'REMOVE_FROM_CART':
       let updatedCartItem = state.cart_Items.filter(
         (element) => element.id !== action.payload.id
@@ -30,11 +60,11 @@ function cartReducer(state, action) {
         , cart_Items: updatedCartItem
 
       }
-      case "CLEAR_CART":
-        return {
-         ...state
-          , cart_Items: []
-        }
+    case "CLEAR_CART":
+      return {
+        ...state
+        , cart_Items: []
+      }
     default:
       return state;
   }
