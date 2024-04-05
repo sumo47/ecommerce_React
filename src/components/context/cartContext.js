478d9ec3a02866327aabf169a42a83f045b60264
sumit kumar <sumokhan76342@gmail.com>
2024-04-05 21:05:14 +0530

    added functionality - persistent cart Items

--- src/components/context/cartContext.js
+++ src/components/context/cartContext.js
@@ -1,10 +1,19 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer, useEffect } from 'react';
 import reducer from '../reducer/cartReducer'
 
 const cartContext = createContext()
 
+const localStorageCartItem = () => {
+    let cart_Items = JSON.parse(localStorage.getItem("cart_items"))
+    if (cart_Items === null) {
+        cart_Items = []
+        
+    }
+    return cart_Items
+}
+
 const initialState = {
-    cart_Items: [],
+    cart_Items: localStorageCartItem(),
     total_Amount: 0,
     total_Item: 0,
     shipping_Fee: 50000
@@ -17,16 +26,20 @@ const CartProvider = ({ children }) => {
     const addToCart = (id, color, amount, product) => {
         dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } })
     }
-    const removeItem  = (id) =>{
+    const removeItem = (id) => {
         dispatch({ type: "REMOVE_FROM_CART", payload: { id } })
         console.log("function")
     }
+    useEffect(() => {
+        localStorage.setItem("cart_items", JSON.stringify(state.cart_Items))
+    }, [state.cart_Items])
 
-    return <cartContext.Provider value={{ ...state, addToCart, removeItem}}>{children}</cartContext.Provider>
+
+    return <cartContext.Provider value={{ ...state, addToCart, removeItem }}>{children}</cartContext.Provider>
 }
 
 
- const useGlobalCartProvider = () => {
+const useGlobalCartProvider = () => {
     return useContext(cartContext)
 }
-export {useGlobalCartProvider, CartProvider}
+export { useGlobalCartProvider, CartProvider }
